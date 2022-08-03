5d2675285e0490586c531d548cf190c9e0f3ae76
abdullah-yasir-cowlar <abdullah.yasir.cowlar@gmail.com>
2022-08-03 13:14:24 +0500

    added user login and logout

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -19,36 +19,51 @@ const deleteTask = ({ _id }) => TasksCollection.remove(_id)
 export const App = () => {
   const [hideCompleted, setHideCompleted] = useState(false)
   const hideCompletedFilter = { isChecked: { $ne: true } }
-
   const user = useTracker(() => Meteor.user())
-  const tasks = useTracker(() =>
-    TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
-      sort: { createdAt: -1 },
-    }).fetch()
-  )
+  const userFilter = user ? { userId: user._id } : {}
 
-  const pendingTasksCount = useTracker(() =>
-    TasksCollection.find(hideCompletedFilter).count()
-  )
+  const pendingOnlyFilter = { ...hideCompletedFilter, ...userFilter }
 
-  const pendingTasksTitle = `${
-    pendingTasksCount ? ` (${pendingTasksCount})` : ''
-  }`
+  const tasks = useTracker(() => {
+    if (!user) {
+      return []
+    }
+
+    return TasksCollection.find(
+      hideCompleted ? pendingOnlyFilter : userFilter,
+      {
+        sort: { createdAt: -1 },
+      }
+    ).fetch()
+  })
+
+  const pendingTasksCount = useTracker(() => {
+    if (!user) {
+      return 0
+    }
+
+    return TasksCollection.find(pendingOnlyFilter).count()
+  })
+
+  const logout = () => Meteor.logout()
 
   return (
     <div className="app">
       <header>
         <div className="app-bar">
           <div className="app-header">
-            <h1>📝️ To Do List {pendingTasksTitle}</h1>
+            <h1>📝️ To Do List {pendingTasksCount}</h1>
           </div>
         </div>
       </header>
 
       <div className="main">
         {user ? (
-          <Fragment>
-            <TaskForm />
+          <>
+            <div className="user" onClick={logout}>
+              {user.username} 🚪
+            </div>
+            <TaskForm user={user} />
 
             <div className="filter">
               <button onClick={() => setHideCompleted(!hideCompleted)}>
@@ -66,7 +81,7 @@ export const App = () => {
                 />
               ))}
             </ul>
-          </Fragment>
+          </>
         ) : (
           <LoginForm />
         )}

894cfc69fe700df894f9db9aa634a4bbddee1715
abdullah-yasir-cowlar <abdullah.yasir.cowlar@gmail.com>
2022-08-03 12:38:50 +0500

    added delete function

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -13,6 +13,8 @@ const toggleChecked = ({ _id, isChecked }) => {
   })
 }
 
+const deleteTask = ({ _id }) => TasksCollection.remove(_id)
+
 export const App = () => {
   const tasks = useTracker(() =>
     TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch()
@@ -23,7 +25,12 @@ export const App = () => {
       <h1>Welcome to Meteor!</h1>
       <TaskForm />
       {tasks.map(task => (
-        <Task key={task._id} task={task} onCheckboxClick={toggleChecked} />
+        <Task
+          key={task._id}
+          task={task}
+          onCheckboxClick={toggleChecked}
+          onDeleteClick={deleteTask}
+        />
       ))}
     </div>
   )

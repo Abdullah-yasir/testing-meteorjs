c104b58c7307f0112242fa9156e6988c745d78e3
Abdullah-yasir <56272322+Abdullah-yasir@users.noreply.github.com>
2023-06-25 15:13:53 +0500

    test

--- imports/api/tasksMethods.tests.js
+++ imports/api/tasksMethods.tests.js
@@ -1,8 +1,8 @@
 import { Meteor } from 'meteor/meteor'
 import { Random } from 'meteor/random'
 import { mockMethodCall } from 'meteor/quave:testing'
-import { assert } from 'chai'
 import { TasksCollection } from '/imports/db/TasksCollection'
+import { assert } from 'chai'
 import '/imports/api/tasksMethods'
 
 if (Meteor.isServer) {
